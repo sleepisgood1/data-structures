5f5bf01f919596a9e621b5c1aaccc4e9e29d2ecb
sleepisgood1 <jae1994krw@gmail.com>
2021-11-10 13:29:39 -0800

    first functional queue was created

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,24 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var addedcount = 0;
+  var removedcount = 0;
   someInstance.enqueue = function(value) {
+    storage[addedcount] = value;
+    addedcount ++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[removedcount];
+    if ((addedcount - removedcount) !== 0) {
+      delete storage[removedcount];
+      removedcount ++;
+    }
+    return temp;
   };
 
   someInstance.size = function() {
+    return addedcount - removedcount;
   };
 
   return someInstance;
