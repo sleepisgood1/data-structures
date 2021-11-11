48e7c51dfeb6189096b0d87036d621ca3e3c3129
sleepisgood1 <jae1994krw@gmail.com>
2021-11-10 16:52:51 -0800

    Functional queue and stack done

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,22 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var count = 0;
   someInstance.push = function(value) {
+    storage[count] = value;
+    count ++;
   };
 
   someInstance.pop = function() {
+    var poppedItem = storage[count - 1];
+    if (count !== 0) {
+      count --;
+    }
+    return poppedItem;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
