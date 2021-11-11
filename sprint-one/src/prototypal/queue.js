26134ea37b4de91143d7f6c64f80b123b528919f
sleepisgood1 <jae1994krw@gmail.com>
2021-11-10 20:03:10 -0800

    compeleted functional-shared and protypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.count = 0;
+  newQueue.indextodelete = 0;
+  return newQueue;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+queueMethods.dequeue = function() {
+  var dequeuevalue = this.storage[this.indextodelete];
+  delete this.storage[this.indextodelete];
+  if (this.indextodelete < this.count) {
+    this.indextodelete++;
+  }
+  return dequeuevalue;
+};
+queueMethods.size = function() {
+  var difference = this.count - this.indextodelete;
+  return difference;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.count = 0;
+  return newStack;
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
 var stackMethods = {};
 
-
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+stackMethods.pop = function() {
+  var poppeditem = this.storage[this.count - 1];
+  delete this.storage[this.count - 1];
+  if (this.count !== 0) {
+    this.count --;
+  }
+  return poppeditem;
+};
+stackMethods.size = function() {
+  return this.count;
+};
