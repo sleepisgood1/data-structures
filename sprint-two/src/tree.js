6fa4f37e57502821271db2a0a36b1a76d7b677ed
sleepisgood1 <jae1994krw@gmail.com>
2021-11-13 09:00:37 -0800

    almost done with graph

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -18,44 +18,50 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  var contain = false;
+  // var contain = false;
 
-  var checkContains = function(tree) {
-    var currentContainTree = tree;
+  // var checkContains = function(tree) {
+  //   var currentContainTree = tree;
 
-    if (currentContainTree.value === target) {
-      contain = true;
-    }
+  //   if (currentContainTree.value === target) {
+  //     contain = true;
+  //   }
 
-    for (var i = 0; i < currentContainTree.children.length; i++) {
-      checkContains(currentContainTree.children[i]);
-    }
-  };
+  //   for (var i = 0; i < currentContainTree.children.length; i++) {
+  //     checkContains(currentContainTree.children[i]);
+  //   }
+  // };
 
-  checkContains(this);
+  // checkContains(this);
 
-  return contain;
+  // return contain;
+
+
   //check if value of tree (this.value) is equal to target. if it is return true.
-  // var targetExists = false;
-  // if (this.value === target) {
-  //   targetExists = true;
-  // }
-  // //foreach item in the tree.children, check if tree.value is equal to target
-  // if (this.children.length !== 0) {
-  //   this.children.forEach(function(item) {
-  //     item.contains(target);
-  //   });
-  // }
-  // return false;
-  //if it is return true;
-  //return the value of rcursing contains on item.chiledren
-  //return false
-
+  if (this.value === target) {
+    return true;
+  }
+  //foreach item in the tree.children, check if tree.value is equal to target
+  for (i = 0; i < this.children.length; i++) {
+    var currentValue = this.children[i];
+    if (currentValue.contains(target)) {
+      return true;
+    }
+  }
+  // this.children.forEach(function(item) {
+  //   return item.contains(target);
+  // });
+  return false;
 
 };
 
-
-
+// var tree = new Tree;
+// tree.addChild(5);
+// tree.addChild(6);
+// tree.children[0].addChild(7);
+// tree.children[1].addChild(8);
+// console.log(tree.contains(7));
+// console.log(tree.contains(8));
 /*
  * Complexity: What is the time complexity of the above functions?
  */
