d97ed442ca32ccc6a9666a65f6be1e0d5f7da4a4
sleepisgood1 <jae1994krw@gmail.com>
2021-11-12 16:49:52 -0800

    done until tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,67 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create var = new Node(value)
+    var currentNode = new Node(value);
+    //add node to list (count??? as key???  ) cross out
+
+    //if head is null, set head's value equal to current node (var)
+    if (list.head === null) {
+      list.head = currentNode;
+    }
+    if (!list.head === list.tail) {
+      list.tail.next = currentNode;
+    }
+    //change tail's next node to current node
+    list.tail = currentNode;
+    //set tail equal to current node (var)
+    if (list.head.next === null && list.head !== list.tail) {
+      list.head.next = list.tail;
+    }
+    //i dont htink head.next is same as tail.next - we never set the head.next value as anything
   };
 
   list.removeHead = function() {
+    //store list.head.next's node
+    if (list.head === null) {
+      return null;
+    }
+    var removedValue = list.head.value;
+    if (list.head === list.tail) {
+      list.head = null;
+      list.tail = null;
+      return removedValue;
+    } else {
+    //change ucrrent head to become the next value
+      var tempNode = list.head.next;
+      list.head = tempNode;
+      return removedValue;
+    // return removedvalue;
+    }
   };
 
   list.contains = function(target) {
+    var contain = false;
+
+    var checkContains = function (node) {
+      var temporaryCurrentNode = node;
+      if (temporaryCurrentNode.value === target) {
+        contain = true;
+      } else {
+        if (temporaryCurrentNode.next) {
+          checkContains(temporaryCurrentNode.next);
+        }
+      }
+    };
+
+    checkContains(list.head);
+
+    return contain;
+
+    //check list.head
+    //if node.vlaue of head is target, return true
+    //else nove.enxt of head and check using contains. (recursive)
+    //return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,55 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //create a new tree with this value
+  var currentTree = new Tree(value);
+  //push this tree onto the children list
+  this.children.push(currentTree);
 };
 
 treeMethods.contains = function(target) {
+  var contain = false;
+
+  var checkContains = function(tree) {
+    var currentContainTree = tree;
+
+    if (currentContainTree.value === target) {
+      contain = true;
+    }
+
+    for (var i = 0; i < currentContainTree.children.length; i++) {
+      checkContains(currentContainTree.children[i]);
+    }
+  };
+
+  checkContains(this);
+
+  return contain;
+  //check if value of tree (this.value) is equal to target. if it is return true.
+  // var targetExists = false;
+  // if (this.value === target) {
+  //   targetExists = true;
+  // }
+  // //foreach item in the tree.children, check if tree.value is equal to target
+  // if (this.children.length !== 0) {
+  //   this.children.forEach(function(item) {
+  //     item.contains(target);
+  //   });
+  // }
+  // return false;
+  //if it is return true;
+  //return the value of rcursing contains on item.chiledren
+  //return false
+
+
 };
 
 
